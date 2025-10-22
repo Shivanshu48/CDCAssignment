0be30c51bc2120c8e96f8f58b3d1298f8d0c0f35
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2025-10-22 16:31:37 +0530

    Create code.cpp

---
+++ Binary Search/GFG - Agressive Cows/code.cpp
@@ -0,0 +1,40 @@
+class Solution {
+  public:
+    bool isPossible(vector<int> &stalls, int n, int k,int minAllowed){
+        int cows = 1, lastStallPos = stalls[0];
+        
+        for(int i=1;i<n;i++){
+            if(stalls[i]-lastStallPos >= minAllowed){
+                cows++;
+                lastStallPos = stalls[i];
+            }
+            
+            if(cows == k){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    int aggressiveCows(vector<int> &stalls, int k) {
+        // code here
+        int n = stalls.size();
+        sort(stalls.begin(), stalls.end());
+        
+        int st = 1, end = stalls[n-1]-stalls[0], ans = -1;
+        
+        while(st <= end){
+            int mid = st+(end-st)/2;
+            
+            if(isPossible(stalls, n, k, mid)){
+                ans = mid;
+                st = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+        
+};
