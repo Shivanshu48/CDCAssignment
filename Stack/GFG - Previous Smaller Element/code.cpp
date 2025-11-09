00af9cca161465804bfb41f892a1ee453a14dc1f
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2025-11-10 01:53:20 +0530

    Create code.cpp

---
+++ Stack/GFG - Previous Smaller Element/code.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    vector<int> prevSmaller(vector<int> arr) {
+        //  code here
+        stack<int> s;
+        vector<int> ans(arr.size(), 0);
+        for(int i=0; i<arr.size(); i++){
+            while(s.size()>0 && s.top() >= arr[i]){
+                s.pop();
+            }
+
+            if(s.empty()){
+                ans[i] = -1;
+            }
+            else{
+                ans[i] = s.top();
+            }
+            s.push(arr[i]);
+        }
+        return ans;
+    }
+};
